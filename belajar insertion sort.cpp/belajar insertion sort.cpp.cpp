1c21ff64cc4f42dd130d3018caf0c7a6a4ea9b51
Nadilaaaa12 <nadilazaskiaauliaaulia@gmail.com>
2023-03-23 11:28:09 +0700

    membuat prosedur void display

--- belajar insertion sort.cpp/belajar insertion sort.cpp.cpp
+++ belajar insertion sort.cpp/belajar insertion sort.cpp.cpp
@@ -57,3 +57,23 @@ void insertionsort() {                 // Procedure Insertionsort
 
 	}
 }
+
+void display() {                // Procedur Display
+	cout << endl;                                                 // Output baris kosong
+	cout << "\n============================================" << endl;  // Output ke layar
+	cout << "Element Array yang telah tersusun" << endl;       // Output ke layar
+	cout << "==================================" << endl;      // Output ke layar
+
+	for (int j = 0; j < n; j++) {                // Looping dengan j dimulai dari 0 hingga n-1
+		cout << arr[j] << endl;               // Output ke layar
+	}
+	cout << endl;                             // Output baris kosong
+}
+
+int main()
+{
+	input();             // memanggil input
+	insertionsort()      // memanggil insertionsort
+	display()            // memanggil display
+}
+
