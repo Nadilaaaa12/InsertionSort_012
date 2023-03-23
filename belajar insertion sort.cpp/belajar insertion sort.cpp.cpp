6ba2d6652ce10bc806a2d395471eb106326436cd
Nadilaaaa12 <nadilazaskiaauliaaulia@gmail.com>
2023-03-23 10:47:30 +0700

    hfhfhfyj

--- belajar insertion sort.cpp/belajar insertion sort.cpp.cpp
+++ belajar insertion sort.cpp/belajar insertion sort.cpp.cpp
@@ -4,7 +4,6 @@ using namespace std;
 int arr[20];                              // Membuat Array dengan panjang data 20
 int n;                                    // Membuat Variable inputan n
 
-
 void input() {            // Procedure Input
 	while (true)
 	{
@@ -20,5 +19,13 @@ void input() {            // Procedure Input
 		}
 	}
 	cout << endl;                               // Membuat jarak per baris program
-	cout << "=======================" << endl;  // Membuat tampilan susunan data element
+	cout << "======================" << endl;  // Membuat tampilan susunan data element
+	cout << "Masukkan Element Array" << endl;
+	cout << "======================" << endl;
+
+	for (int i = 0; i < n; i++)                // Menggunakan perulangan for untuk menyimpan data pada array
+	{
+		cout << "Data ke-" << (i + 1) << ": "; // Memasukkan atau menginputkan nilai data n
+		cin >> arr[i];                         // Menyimpan nilai data n kedalam array arr
+	}
 }
